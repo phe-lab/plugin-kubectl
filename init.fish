b271b13d6f8861c2be5837e58317662dc9c275e6
Nguyen Huu Kim <13513658+kimyvgy@users.noreply.github.com>
2024-03-18 14:43:14 +0700

    refactor: update for docker logs

--- init.fish
+++ init.fish
@@ -29,7 +29,7 @@ if which docker > /dev/null
   abbr -a -g dk "docker"
   abbr -a -g dke "docker exec -it"
   abbr -a -g dkps "docker ps --format 'table {{.ID}}\t{{.Names}}\t{{.Status}}'"
-  abbr -a -g dkl "docker logs"
+  abbr -a -g dkl "docker logs -f --tail 300"
 
   abbr -a -g dkx "dockerctx"
 end
